62b2f49880697612101d714609d39f64ce74aaf1
Ku <polorbear520@gmail.com>
2022-12-05 17:45:32 +0800

    新增註冊js

--- js/index.js
+++ js/index.js
@@ -9,4 +9,58 @@ $(document).ready(function () {
     });
 });
 
+/* sweetAlert 成功 */
+function sweet2Success(title, text, timer = 1500) {
+    Swal.fire({
+        icon: 'success',
+        title: title,
+        text: text,
+        showConfirmButton: true,
+        timer: timer
+    })
+}
+/* sweetAlert 失敗*/
+function sweet2Error(title, text) {
+    Swal.fire({
+        icon: 'warning',
+        title: title,
+        text: text,
+        showConfirmButton: true,
+        timer: 2000
+    })
+}
+
+/* 註冊登入 */
+const registerAccount = document.querySelector(".registerAccount");
+const registerPassword = document.querySelector(".registerPassword");
+const registerBtn = document.querySelector(".registerBtn");
+const base_url = `http://localhost:3000`;
+
+//aveUserToLocal()
+function saveUserToLocal({ accessToken, user }) {
+    localStorage.setItem('token', accessToken);
+    localStorage.setItem('userId', user.id);
+}
+
+// #Step-1: `POST` data to API
+// registerBtn.addEventListener("click", register);
+function register() {
+    if (registerAccount.value === "" || registerPassword.value === "") {
+        sweet2Error('請輸入完整資料');
+        return;
+    }
+    let obj = {};
+    obj.email = registerAccount.value.trim();
+    obj.password = registerPassword.value.trim();
+    axios.post(`${base_url}/register`, obj)
+        .then(function (response) {
+            console.log('hello');
+            
+            sweet2Success(`註冊成功`);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+register()
 
