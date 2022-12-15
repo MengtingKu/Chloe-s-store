80e4e0e9e2b92ee2e0f2965a2a1f1e5e5bdadacf
Ku <polorbear520@gmail.com>
2022-12-15 14:05:19 +0800

    no message

--- js/index.js
+++ js/index.js
@@ -179,9 +179,6 @@ function addLike(e) {
             if (error.response.data === "jwt expired") {
                 sweet2Error(`時間到！請登出後重新登入！`)
             }
-            if (error.response.data === "jwt malformed") {
-                sweet2Error(`請登入後操作！`)
-            }
         });
 }
 
